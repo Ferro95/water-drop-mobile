a1122c5faddd883c4e8cb8a6fb712bfc824bc4f9
Ferro95 <393262717@qq.com>
2023-04-09 21:32:44 +0800

    移除eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd-mobile';
+
 function App() {
-  return <h1>Some content</h1>;
+  return (
+    <Form
+      footer={
+        <Button block type="submit" color="primary" size="large">
+          提交
+        </Button>
+      }
+    >
+      <Form.Item name="name" label="姓名">
+        <Input />
+      </Form.Item>
+      <Form.Item name="desc" label="描述">
+        <Input />
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './utils/apollo';
+import { ConfigProvider, ErrorBlock } from 'antd-mobile';
+import zhCN from 'antd-mobile/es/locales/zh-CN';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ApolloProvider client={client}>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </ApolloProvider>
 );
